1b8b53be9ec551337fbd4f2d3aba12343801bfb0
Pierre Fenoll <pierrefenoll@gmail.com>
2017-01-16 14:43:56 +0100

    nknwn: display vsn of cross used

--- ci/install.sh
+++ ci/install.sh
@@ -11,7 +11,8 @@ main() {
         target=x86_64-apple-darwin
     fi
 
-    tag="$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross |cut -d/ -f3 | tail -n1)"
+    local tag="$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross | cut -d/ -f3 | tail -n1)"
+    echo cross version: $tag
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
